4c2abeecccf63b7f7393751d9bdd501b5ccad724
Aaditdot-1234 <kumud2196@gmail.com>
2026-04-04 15:48:37 +0530

    added another section in home page.

--- Frontend/src/app/Auth/auth.service.ts
+++ Frontend/src/app/Auth/auth.service.ts
@@ -1,9 +1,10 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-
-  constructor() { }
+  private apiUrl = 'http://localhost:3000/auth';
+  constructor(private http: HttpClient) { }
 }
--- Frontend/src/app/home/home.component.ts
+++ Frontend/src/app/home/home.component.ts
@@ -1,11 +1,43 @@
 import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+  categories = [
+    {
+      name: 'Home Appliances',
+      image: '/category1.png',
+      color: 'lightblue'
+    },
+    {
+      name: 'Furniture',
+      image: '/category2.png',
+      color: 'lightgreen'
+    },
+    {
+      name: 'Accessories',
+      image: '/category3.png',
+      color: 'lightyellow'
+    },
+    {
+      name: 'Fashion',
+      image: '/category4.png',
+      color: 'lightpink'
+    },
+    {
+      name: 'Arts & Crafts',
+      image: '/category5.png',
+      color: 'lightcoral'
+    },
+    {
+      name: 'Beauty',
+      image: '/category6.png',
+      color: 'lightgray'
+    }
+  ]
 }
