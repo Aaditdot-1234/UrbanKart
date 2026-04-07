e374c6b58d9658f1641cdf591db893371843a315
Aaditdot-1234 <kumud2196@gmail.com>
2026-04-07 09:50:55 +0530

    Added navbar.

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -2,15 +2,16 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ToastService } from './shared/services/toast.service';
 import { AsyncPipe } from '@angular/common';
+import { NavbarComponent } from './shared/components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, AsyncPipe],
+  imports: [RouterOutlet, AsyncPipe, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Frontend';
 
-  constructor(public toast: ToastService){}
+  constructor(public toast: ToastService) { }
 }
--- Frontend/src/app/shared/components/navbar/navbar.component.ts
+++ Frontend/src/app/shared/components/navbar/navbar.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+  onHover: boolean = false;
+  isScrolled: boolean = false;
 
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const scroll = 50;
+    this.isScrolled = window.scrollY > scroll;
+  }
+
+  @HostListener('window:mouseenter', [])
+  onMouseEnter() {
+    this.onHover = true;
+  }
+
+  @HostListener('window:mouseleave', [])
+  onMouseLeave() {
+    this.onHover = false;
+  }
 }
